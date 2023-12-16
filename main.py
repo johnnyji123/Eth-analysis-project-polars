1b0bb9146e22c591f17b2c9310e7074e1f68feb9
Johnny Ji <jj_johnny97@hotmail.com>
2023-12-16 13:02:18 +0000

    created pct_change_price_df

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ df = df.with_columns(
     )   
 
 
-df = df.groupby("Date").agg(
+avg_df = df.group_by("Date").agg(
         pl.col("Open").mean(),
         pl.col("High").mean(),
         pl.col("Low").mean(),
@@ -29,12 +29,12 @@ df = df.groupby("Date").agg(
     )
 
 
-df = df.sort("Date", descending = False)
+avg_df = avg_df.sort("Date", descending = False)
 
 pl.Config(set_fmt_float = "full")
 
     
-df = df.with_columns(
+avg_df = avg_df.with_columns(
     pl.col("Open").round(decimals = 2),
     pl.col("High").round(decimals = 2),
     pl.col("Low").round(decimals = 2),
@@ -49,22 +49,51 @@ df = df.with_columns(
 # Pct increase is still postivie but decreasing
 # From 2022-2023 pct change is negative - indicating a decrease in volume
 # 2019 Had the greatest increase in volume, 2023 had the greatest decrease in volume  
-volume_change_over_time = df.select(
+volume_change_over_time_df = avg_df.select(
     pl.col("Date"),
     pl.col("Volume"),
     (pl.col("Volume").pct_change() * 100).alias("pct change volume")
     )
 
-
+    
 
 # price trends over time
-price_over_time = sns.lineplot(x = "Date", y = "Close", data = df)
+price_over_time = sns.lineplot(x = "Date", y = "Close", data = avg_df)
 price_over_time.set(title = "Price over time", ylabel = "Closing price")
 
+# Annotated the price on the lineplot
+# Price has steadily increased from 2018 to 2020
+# From 2020-2021 there was a sharp increase in price
+# Price gradually decreasing from 2021-2023
+def plot_price_over_time(df):
+    for year in df["Date"].unique().to_list():
+        point_coordinates = df.filter(pl.col("Date") == year)
+        for row in point_coordinates.to_pandas().itertuples(index = False):
+            plt.text(row.Date, row.Close, f"{row.Close}", ha = "left", va = "bottom")
 
-for year in df["Date"].unique():
-    point_coordinates = df.filter(pl.col("Date") == year)
-    for row in point_coordinates.to_pandas().itertuples(index = False):
-        plt.text(row.Date, row.Close, f"{row.Close}", ha = "left", va = "bottom")
+            
+#plot_price_over_time(avg_df)     
+    
+# 2021 had the greatest pct change in price 
+# In 2022 and 2023 price has been decreasing
+pct_change_price_df = avg_df.select(
+    pl.col("Date"),
+    pl.col("Close"),
+    (pl.col("Close").pct_change() * 100).alias("pct_change")
+    
+    )      
+
+
         
-plt.show()
+# Measuring volatility based on standard deviation and mean
+volatility_df = avg_df.with_columns(
+        pl.col("Open").round(decimals = 2),
+        pl.col("High").round(decimals = 2),
+        pl.col("Low").round(decimals = 2),
+        pl.col("Close").round(decimals = 2),
+        pl.col("Adj Close").round(decimals = 2),
+        pl.col("Voume").round(decimals = 2)
+    
+    )
+
+volatility_df
